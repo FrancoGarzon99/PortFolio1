b37d9685f263f9bd1608daddfddb39232f4ef690
FrancoGarzon99 <57675375+FrancoGarzon99@users.noreply.github.com>
2020-05-25 21:46:59 -0300

    Portfolio 1
    Proyecto de Maquetado, realizado con Html & Css , Boostrap y JavaScript para algunas interactividades basicas.

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+    
+  });
+
+
